bfbdfca40caa60d71794061883608dd378627513
TheAIBot <andreasuranus@hotmail.com>
2018-10-07 03:05:50 +0200

    forgot to add code so it will work in bases that used an older version of the mod

--- control.lua
+++ control.lua
@@ -1,9 +1,19 @@
 local pipeCleaner = require("pipe-cleaner")
 
 script.on_init(function()
+	reset()
+end)
+
+script.on_configuration_changed(function(data)
+	if data.mod_changes and data.mod_changes["Pipe-Manager"] then
+		reset()
+	end
+end)
+
+function reset()
 	global.jobsStartedCount = 1
 	global.jobs = {}
-end)
+end
 
 script.on_event(defines.events.on_built_entity      , pipeCleaner.checkCreateNewCleanerJob)
 script.on_event(defines.events.on_robot_built_entity, pipeCleaner.checkCreateNewCleanerJob)
